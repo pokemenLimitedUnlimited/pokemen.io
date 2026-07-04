2799f001ddafa5ebd52f3408ae1b9a8c962c8701
pokemenLimitedUnlimited <pokemenlimitedunlimited@gmail.com>
2026-07-04 23:57:17 +0100

    Delete HTML + CSS directory

--- HTML + CSS/Javascript/index.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById("hamburgerMenuDiv");
-const sidebar = document.getElementById("sideBarDiv");
-
-// Click to open and click again to close
-hamburger.addEventListener("click", () => {
-  sidebar.classList.toggle("show-sidebar");
-});
